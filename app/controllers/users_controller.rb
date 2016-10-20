18536ca8b3ef3a61b0fa4e65ed5b54c9dffd2b58
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2016-10-20 10:31:23 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,10 +45,24 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = t ".title"
+    @user  = User.find params[:id]
+    @users = @user.following.paginate page: params[:page]
+    render "show_follow"
+  end
+
+  def followers
+    @title = t ".title"
+    @user  = User.find params[:id]
+    @users = @user.followers.paginate page: params[:page]
+    render "show_follow"
+  end
+
   private
   def user_params
-  	params.require(:user).permit(:name, :email, :password,
-                               :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
   end
 
   def admin_user
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module SampleApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
